3f99b08efa911c687c83e2101d41ecd05f241b7c
EternoImpietri <pierre.ominetti@gmail.com>
2023-02-17 13:58:17 +0900

    IDK

--- pyNozzle/tests/test_nozzle.py
+++ pyNozzle/tests/test_nozzle.py
@@ -1,8 +1,7 @@
 from pyNozzle.nozzle import Nozzle, ConicalNozzle
-from pyNozzle.flow import Flow
-from pyNozzle.substance import Substance
-import matplotlib.pyplot as plt
-import numpy as np
+
+
+
 
 def test_nozzle_plot():
     nozzle = ConicalNozzle(5, 3, 7, 10, 30, "mm")
