8d48e113027d9567627a7997c9c19b8d97236438
EternoImpietri <pierre.ominetti@gmail.com>
2023-02-24 15:09:45 +0900

    Substance class curves added step

--- pyNozzle/substance.py
+++ pyNozzle/substance.py
@@ -70,30 +70,35 @@ class Substance :
     ):
         return CP.CoolProp.PropsSI('P', 'T', T, 'Q', 1 ,self.name)
 
-    def get_vaporization_curve(self):
-        T = np.arange(self.T_triple,self.T_crit,1)
+    def get_vaporization_curve(self,
+        step : float = 1
+    ):
+        T = np.arange(self.T_triple,self.T_crit,step)
         P = np.array([CP.CoolProp.PropsSI('P', 'T', t, 'Q', 1 ,self.name) for t in T])
         return T, P
     
-    def get_melting_curve(self):
+    def get_melting_curve(self,
+        step : float = 1000
+    ):
         s = CP.AbstractState("HEOS", self.name)
-        P = np.logspace(math.log10(self.p_triple + 0.01), math.log10(s.keyed_output(CP.iP_max)),1000)
+        P = np.logspace(math.log10(self.p_triple + 0.01), math.log10(s.keyed_output(CP.iP_max)),step)
         T = np.array([s.melting_line(CP.iT, CP.iP, p) for p in P])
         return T, P
 
-    def get_sublimation_curve(self):
+    def get_sublimation_curve(self,
+        step : float = 1
+    ):
         if self.name == "Water" :
             # https://doi.org/10.1063/1.555947
             # https://doi.org/10.1063/1.1461829
 
-            T = np.arange(190, self.T_triple, 1)
+            T = np.arange(190, self.T_triple, step)
             p_n = 611.657
             T_n = 273.16
             Theta = T/T_n
             a_1 = -13.928169
             a_2 = 34.7078238
             P = np.array([p_n*math.exp(a_1*(1-t**(-1.5)) + a_2*(1-t**(-1.25))) for t in Theta])
-            
             return T, P
         else :
             raise ValueError("No sublimation curve for this substance")
