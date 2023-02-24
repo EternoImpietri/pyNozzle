9a6d45e0ade33b111304096ee660d5718e57ff94
EternoImpietri <pierre.ominetti@gmail.com>
2023-02-24 16:39:12 +0900

    Better isentropic flow calculation for iteration and included compression part

--- pyNozzle/flow.py
+++ pyNozzle/flow.py
@@ -1,5 +1,9 @@
 import scipy.optimize as opt
 import math
+import matplotlib.pyplot as plt
+import numpy as np
+from alive_progress import alive_bar
+
 
 from pyNozzle.nozzle import Nozzle, ConicalNozzle
 from pyNozzle.substance import Substance
@@ -8,14 +12,14 @@ class Flow :
     
     def __init__(
         self, 
-        T_0 : float, 
-        p_0 : float, 
+        T_c : float, 
+        p_c : float, 
         nozzle : Nozzle,
         substance : Substance
     ):
         
-        self.T_0 = T_0
-        self.p_0 = p_0
+        self.T_c = T_c
+        self.p_c = p_c
         self.nozzle = nozzle
         self.substance = substance
         
@@ -23,34 +27,33 @@ class IsentropicFlow(Flow) :
 
     def __init__(
         self, 
-        T_0 : float, 
-        p_0 : float, 
+        T_c : float, 
+        p_c : float, 
         nozzle : Nozzle,
         substance : Substance
     ):
         
-        super().__init__(T_0, p_0, nozzle, substance)
+        super().__init__(T_c, p_c, nozzle, substance)
     
     def simple_calculation(self, 
         p_a : float = 0,
     ):
-        print("Simple calculation - Start")
 
-        p_0 = self.p_0
-        T_0 = self.T_0
+        p_c = self.p_c
+        T_c = self.T_c
         A_t = self.nozzle.A_t
         A_e = self.nozzle.A_e
         
-        k = self.substance.get_heat_capacity_ratio(T_0)
+        k = self.substance.get_heat_capacity_ratio(T_c)
         R = self.substance.R
 
         # ----------------------------- Throat conditions ---------------------------- #
        
-        p_t = p_0*(2/(k+1))**(k/(k-1))
-        T_t = 2*T_0/(k+1)
+        p_t = p_c*(2/(k+1))**(k/(k-1))
+        T_t = 2*T_c/(k+1)
         a_t = math.sqrt(k*R*T_t)
-        m_dot = A_t*p_0*k*math.sqrt(((2/(k+1))**((k+1)/(k-1)))/(k*R*T_0))
-        c_star = p_0*A_t/m_dot
+        m_dot = A_t*p_c*k*math.sqrt(((2/(k+1))**((k+1)/(k-1)))/(k*R*T_c))
+        c_star = p_c*A_t/m_dot
 
         # ---------------------------- Objective Function ---------------------------- #
 
@@ -63,9 +66,9 @@ class IsentropicFlow(Flow) :
         
         # ------------------------------ Exit conditions ----------------------------- #
 
-        p_e = p_0*pressure_ratio.x
-        M_e = math.sqrt(2*((p_0/p_e)**((k-1)/k)-1)/(k-1))
-        T_e = T_0/(1+0.5*(k-1)*M_e**2)
+        p_e = p_c*pressure_ratio.x
+        M_e = math.sqrt(2*((p_c/p_e)**((k-1)/k)-1)/(k-1))
+        T_e = T_c/(1+0.5*(k-1)*M_e**2)
         a_e = math.sqrt(k*R*T_e)
         v_e = a_e*M_e
 
@@ -74,11 +77,10 @@ class IsentropicFlow(Flow) :
         F_1 = m_dot*v_e*self.nozzle.correction
         F_2 = (p_e-p_a)*A_e
         F_total = (F_1 + F_2)
-        C_F = F_total/(A_t*p_0)
+        C_F = F_total/(A_t*p_c)
 
         Isp = F_total/(m_dot*9.80665)
 
-        print("Simple calculation - End")
         return F_total, Isp, m_dot, T_e, p_e
 
     def iterative_simple_calculation(self,
@@ -88,26 +90,74 @@ class IsentropicFlow(Flow) :
         if type(self.nozzle) is Nozzle:
             raise AssertionError("Must specify which kind of nozzle")
         
-        p = [self.p_0]
-        T = [self.T_0]
-        x, y = self.nozzle.get_envelope_divergent(points)
-        for i in range(points):
-            self.nozzle.D_e = 2*y[i]
-            _, _, _, T_e, p_e = self.simple_calculation(p_a = p_a)
-            p.append(p_e)
-            T.append(T_e)
-        return p, T
+        k = self.substance.get_heat_capacity_ratio(self.T_c)
+        # p = [self.p_c]
+        # T = [self.T_c]
+
+        p = [self.p_c]
+        T = [self.T_c]
+        M = [0]
+        
+        # -------------------------------- Convergent -------------------------------- #
+
+        print("Iterative Convergent")
+
+        x_1, y = self.nozzle.get_envelope_convergent(points)
+        x_1 = np.insert(x_1, 0, 0)
+        with alive_bar(points) as bar :
+            for i in range(points):
+                A = math.pi*y[i]**2
+                def objective_function(x):
+                    return abs((1/x)*(((k+1)/2)**(-(k+1)/(2*(k-1))))*((1+0.5*(k-1)*x*x)**(0.5*(k+1)/(k-1)))-A/self.nozzle.A_t)
+                
+                mach = opt.minimize_scalar(objective_function, 
+                                         method = "bounded",
+                                         bounds = (0, 1))
+                
+                p_e = self.p_c*(1+0.5*(k-1)*mach.x**2)**(-k/(k-1))
+                T_e = self.T_c*(1+0.5*(k-1)*mach.x**2)**(-1)
+                
+                T.append(T_e)
+                p.append(p_e)
+                M.append(mach.x)
+                bar()
+
+        # --------------------------------- Divergent -------------------------------- #
+
+
+        print("Iterative Divergent")
+
+        x_2, y = self.nozzle.get_envelope_divergent(points)
+        with alive_bar(points) as bar :
+            for i in range(points):
+                A = math.pi*y[i]**2
+                def objective_function(x):
+                    return abs((1/x)*(((k+1)/2)**(-(k+1)/(2*(k-1))))*((1+0.5*(k-1)*x*x)**(0.5*(k+1)/(k-1)))-A/self.nozzle.A_t)
+                
+                mach = opt.minimize_scalar(objective_function, 
+                                         method = "bounded",
+                                         bounds = (1, 1e9))
+                
+                p_e = self.p_c*(1+0.5*(k-1)*mach.x**2)**(-k/(k-1))
+                T_e = self.T_c*(1+0.5*(k-1)*mach.x**2)**(-1)
+                
+                T.append(T_e)
+                p.append(p_e)
+                M.append(mach.x)
+                bar()
 
         
+        return np.concatenate((x_1,x_2)), np.array(T), np.array(p), np.array(M)
+
 
 class CFDFlow(Flow):
     # https://nbviewer.org/github/barbagroup/CFDPython/blob/master/lessons/14_Step_11.ipynb
     def __init__(
         self, 
-        T_0 : float, 
-        p_0 : float, 
+        T_c : float, 
+        p_c : float, 
         nozzle : Nozzle,
         substance : Substance
     ):
         
-        super().__init__(T_0, p_0, nozzle, substance)
+        super().__init__(T_c, p_c, nozzle, substance)
--- pyNozzle/tests/test_flow.py
+++ pyNozzle/tests/test_flow.py
@@ -4,15 +4,36 @@ from pyNozzle.flow import Flow, IsentropicFlow
 
 import math
 import matplotlib.pyplot as plt
+import numpy as np
 
 def test_flow():
     water = Substance('Water')
-    nozzle = ConicalNozzle(1e-3*0.5, 1e-3*0.4, 1e-3*2*math.tan(math.radians(30))*4.15+1e-3*0.4, 30, 30, 'm')
+    nozzle = ConicalNozzle(1e-3*1.5, 1e-3*0.4, 1e-3*2*math.tan(math.radians(30))*4.15+1e-3*0.4, 30, 30, 'm')
     flow = IsentropicFlow(303.15,water.get_saturated_pressure(303.15),nozzle,water)
     return flow
 
+
 flow = test_flow()
-# a, b, c, d, e = flow.simple_calculation()
-print(flow.nozzle.D_e)
-p, T = flow.iterative_simple_calculation()
-print(flow.nozzle.D_e)
+
+x, p, T, M = flow.iterative_simple_calculation(points=100)
+
+
+fig, ax = plt.subplots()
+a, b = flow.nozzle.get_envelope_convergent()
+ax.plot(a, b, "k")
+ax.plot(a, -b, "k")
+a, b = flow.nozzle.get_envelope_divergent()
+ax.plot(a, b, "k")
+ax.plot(a, -b, "k")
+
+ax2 = ax.twinx()
+ax2.plot(x, T, "r")
+
+ax3 = ax.twinx()
+# ax3.set_yscale("log")
+ax3.plot(x, p/flow.p_c, "b")
+
+ax4 = ax.twinx()
+ax4.plot(x, M, "g")
+
+plt.show()
